7a4bd97516d7ef03f6982c6a53328d13bb4b86c7
vmmartinezc <marcamarar@gmail.com>
2017-03-22 20:09:19 -0300

    Update: Corrección pepino dulce

--- RScripts/calculo.R
+++ RScripts/calculo.R
@@ -26,7 +26,30 @@ bm<-readRDS('Rbases/bm1.rds')
 
 # Ordeno la base madre por: producto, tienda, fecha
 bmine<-subset.data.frame(bm,bm$productosINE!=0) 
-bmine<-bmine[order(bmine$producto,bmine$productosINE,bmine$tienda,bmine$url),] 
+bmine<-bmine[order(bmine$productosINE,bmine$tienda,as.POSIXct(bmine$fecha)),] 
+
+## 1. Ordeno las variedades y las tiendas:
+
+# a. Creo un ID por tienda:
+bmine$idtienda<-with(bmine, ave(rep(1, nrow(bmine)), bmine$tienda, FUN = seq_along))
+
+# Ordeno la categoía de variedad para completarla. Hay que considerar que en ODEPA existe una categoría, 
+# en algunas bases hay NA y en otras simplemente está vacío.
+bmine$variedad<-ifelse(bmine$variedad==''|is.na(bmine$variedad),0,bmine[,c('variedad')])
+
+## Vamos a completar los 0 con las variedades que encontremos para cada alimento:
+
+## HORTALIZAS FRESCAS
+##=======================================
+
+#Acelga
+bmine$variedad <-
+  
+bmine$producto2<-str_extract(bmine[,c('producto')],'^[a-z].*[a-z]+ ')
+                          
+test<-as.data.frame(bmine[,c('fecha','producto','producto2','tienda','productosINE')])
+
+
 
 print('Fin del calculo')
 
